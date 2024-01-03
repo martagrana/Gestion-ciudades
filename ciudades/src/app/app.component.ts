758f9283b06bc54a90481b0dd8523fd26787d5a6
Marta Grana <marta.grana.fdez@gmail.com>
2024-01-03 10:42:47 +0100

    funcion guardarOActualizar

--- ciudades/src/app/app.component.ts
+++ ciudades/src/app/app.component.ts
@@ -21,27 +21,38 @@ export class AppComponent {
 
   constructor(private cityService: CityService) { }
 
-  guardarCiudad() {
+  /* método para guardar los registros de las ciudades */
+  guardarOActualizarCiudad() {
     const city = this.formCity.value;
-    this.cityService.create(city).subscribe(() => {
-      console.log("Datos guardados");
-    });
-  }
+    const id = this.ciudadSeleccionada ? this.ciudadSeleccionada.$id : null;
 
+    if (id) {
+      // Si id existe y no es null o undefined, es una actualización
+      this.cityService.update(id, city).subscribe(() => {
+        console.log("Datos editados");
+      });
+    } else {
+      // Si id es null o undefined, es una creación
+      this.cityService.create(city).subscribe(() => {
+        console.log("Datos guardados");
+      });
+    }
+  }
+  /* método para listar todos los registros de ciudades */
   cargarCiudades() {
     this.cityService.query().subscribe((cities: City[]) => {
       console.log('Ciudades que vienen desde el servidor', cities);
       this.ciudades = cities;
     })
   }
-
+  /* método para eliminar los registros según el id de la ciudad */
   borrarCiudad(id: string) {
     this.cityService.delete(id).subscribe(() => {
       console.log("ciudad borrada");
       this.cargarCiudades();
     });
   }
-
+  /* método para poder editar los registros según el id de la ciudad */
   editarCiudad(ciudad: City) {
     this.ciudadSeleccionada = ciudad;
     this.formCity.patchValue({
--- ciudades/src/services/city.service.ts
+++ ciudades/src/services/city.service.ts
@@ -10,29 +10,22 @@ export class CityService {
 
   constructor(private httpClient: HttpClient) { }
 
+  /* método para guardar los registros de las ciudades */
   create(city: City): Observable<City> {
     return this.httpClient.post<City>('https://capi-papi.azurewebsites.net/userapis/martatech/cities', city);
   }
-
+  /* método para listar todos los registros de ciudades */
   query(): Observable<City[]> {
     return this.httpClient.get<City[]>('https://capi-papi.azurewebsites.net/userapis/martatech/cities');
   }
-
+  /* método para actualizar los registros según el id de la ciudad */
   update(id: string, city: City): Observable<City> {
     return this.httpClient.put<City>(`https://capi-papi.azurewebsites.net/userapis/martatech/cities/${id}`, city);
   }
-
+  /* método para eliminar los registros según el id de la ciudad */
   delete(id: string): Observable<City> {
     return this.httpClient.delete<City>(`https://capi-papi.azurewebsites.net/userapis/martatech/cities/${id}`);
 
-  }
-
-  saveOrUpdate(id: string, city: City): Observable<City> {
-    if (city.$id) {
-      return this.update(id, city);
-    } else {
-      return this.create(city);
-    }
   }
 
 }
